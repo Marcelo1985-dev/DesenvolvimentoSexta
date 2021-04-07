a66795cbf7f5ecc551194c81095064b62b593446
Marcelo1985-dev <marceloantonio1985@gmail.com>
2021-04-06 22:37:05 -0300

    Primeiro commit

---
+++ src/Cliente.java
@@ -0,0 +1,27 @@
+
+//definir o modelo de cliente dentro da apllicação
+public class Cliente {
+	
+	private String nome;
+	private String cpf;
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+	
+	public void SetCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	public String getCpf() {
+		return this.cpf;
+	}
+	
+	
+	
+
+}
